b412d9bf171b18e9c4a3bd235a4a7cec51bd81e6
Cole <82690016+colerp00@users.noreply.github.com>
2021-04-16 20:07:05 -0700

    Add files via upload

--- L3_2.ino
+++ L3_2.ino
@@ -10,25 +10,31 @@ void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(LED_OUTPUT_PIN, HIGH);
   Serial.println("Pin 3 is HIGH (5V)");
+  digitalWrite(LED_BUILTIN, LOW);
   delay(200);
   
   digitalWrite(LED_OUTPUT_PIN,LOW);
   Serial.println("Pin 3 is LOW (0V)");
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(200);
   
   digitalWrite(LED_OUTPUT_PIN, HIGH);
   Serial.println("Pin 3 is HIGH (5V)");
+  digitalWrite(LED_BUILTIN, LOW);
   delay(200);
   
   digitalWrite(LED_OUTPUT_PIN,LOW);
   Serial.println("Pin 3 is LOW (0V)");
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(200);
   
   digitalWrite(LED_OUTPUT_PIN, HIGH);
   Serial.println("Pin 3 is HIGH (5V)");
+  digitalWrite(LED_BUILTIN, LOW);
   delay(750);
   
   digitalWrite(LED_OUTPUT_PIN,LOW);
   Serial.println("Pin 3 is LOW (0V)");
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(750);
 }
